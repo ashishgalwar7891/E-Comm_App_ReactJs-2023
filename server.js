c225ffbde258e3575d1661461ec0456e43f5c532
ashishgalwar7891 <ashishgalwar7891@gmail.com>
2023-08-09 21:19:48 +0530

    sec commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "./path";
+const path = require("path");
 import {fileURLToPath } from "url";
 
 //configure env
